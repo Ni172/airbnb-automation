fba952712e7136dea090c410d7aaf32707d2e32a
nikolai.nahamov@codecommit-at-789734661575 <nikolai.nahamov@blackswan-technologies.com>
2025-05-24 15:47:17 +0300

    remove pytest.ini plugins and change setup.py

--- setup.py
+++ setup.py
@@ -3,10 +3,10 @@ from setuptools import setup, find_packages
 setup(
     name="pytest-suite-timeout",
     version="0.1",
-    description="A simple Pytest plugin to enforce per-suite timeout",
-    packages=["pytest_suite_timeout"],
+    packages=["pytest_suite_timeout"],  # refers to your plugin package
     entry_points={
-        "pytest11": ["suite_timeout = pytest_suite_timeout"]
+        'pytest11': [
+            'suite-timeout = pytest_suite_timeout',
+        ],
     },
-    install_requires=[],  # No external packages
 )
